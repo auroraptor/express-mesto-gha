dafd9ca2fa9f553417eab6caa93980fb8cfa9705
auroraptor <interestingbutno@yandex.ru>
2022-10-15 21:11:13 +0300

    fix: error handling

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,5 @@
-const { HTTP400Error } = require('../errors/HTTP400Error');
-const { HTTP404Error } = require('../errors/HTTP404Error');
-const { APIError } = require('../errors/APIError');
 const User = require('../models/user');
 const { logError, logNow } = require('../utils/log');
-
-const handleValidationError = (error, req, res) => {
-  logNow(error.name);
-  if (error.name === 'ValidationError') {
-    const path = /`\w+`/gi;
-    return res.send({ message: `Ошибка валидации данных: ${error.message.match(path)}` });
-    // next(new HTTP404Error(`Ошибка валидации данных: ${error.message.match(path)}`));
-  }
-  return error;
-};
-
-const handleCastError = (error, req, res) => {
-  logNow(error.name);
-  if (error.name === 'CastError') {
-    return res.status(400).send({ message: 'Некорректный запрос' });
-  }
-  return error;
-};
-
-const handleNotFoudError = (req, res) => res.status(404).json({ message: `Пользователь с id ${req.params.id} не найден` });
 
 module.exports.createUser = async (req, res) => {
   try {
@@ -30,7 +7,10 @@ module.exports.createUser = async (req, res) => {
     const response = await res.status(200).send({ data: user });
     return response;
   } catch (error) {
-    handleValidationError(error, req, res);
+    if (error.name === 'ValidationError') {
+      const path = /`\w+`/gi;
+      return res.send({ message: `Ошибка валидации данных: ${error.message.match(path)}` });
+    }
     return res.status(500).send({ message: 'Тут что-то не так' });
   }
 };
@@ -50,28 +30,39 @@ module.exports.getUserById = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     if (user === null) {
-      handleNotFoudError(req, res);
+      return res.status(404).json({ message: `Пользователь с id ${req.params.id} не найден` });
     }
-    const response = await res.status(200).send({ data: user });
+    const response = await res.send({ data: user });
     return response;
   } catch (error) {
     logError(error);
-    return handleCastError(error, req, res);
+    if (error.name === 'CastError') {
+      return res.status(400).send({ message: 'Некорректный запрос' });
+    }
+    return res.status(500).send({ message: 'Тут что-то не так' });
   }
 };
 
 module.exports.updateUser = async (req, res) => {
   try {
-    const user = await User.findByIdAndUpdate(req.user._id, req.body, { new: true });
-    if (user === null) {
-      handleNotFoudError(req, res);
-    }
-    const response = await res.status(200).send({ data: user });
+    const user = await User.findByIdAndUpdate(req.user._id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    logNow('reg.body: ', req.body);
+    const response = await res.send({ data: user });
     return response;
   } catch (error) {
     logNow(error.name);
-    handleValidationError(error, req, res);
-    handleCastError(error, req, res);
+    if (error.name === 'ValidationError') {
+      const path = /`\w+`/gi;
+      return res.send({ message: `Ошибка валидации данных: ${error.message.match(path)}` });
+    }
+
+    if (error.name === 'CastError') {
+      return res.status(400).send({ message: 'Некорректный запрос' });
+    }
+
     return res.status(500).send({ message: 'Тут что-то не так' });
   }
 };
@@ -79,15 +70,18 @@ module.exports.updateUser = async (req, res) => {
 module.exports.updateAvatar = async (req, res) => {
   try {
     const { _id } = req.user;
-    const user = await User.findByIdAndUpdate(_id, req.body, { new: true });
+    const user = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
     const response = await res.status(200).send({ data: user });
     return response;
   } catch (error) {
     logNow(error.name);
-    const path = /`\w+`/gi;
     if (error.name === 'ValidationError') {
-      return res.send({ message: new HTTP400Error(`Ошибка валидации данных: ${error.message.match(path)}`).message });
+      const path = /`\w+`/gi;
+      return res.send({ message: `Ошибка валидации данных: ${error.message.match(path)}` });
     }
-    return res.status(500).send({ message: new APIError('Что-то не так').message });
+    return res.status(500).send({ message: 'Тут что-то не так' });
   }
 };
