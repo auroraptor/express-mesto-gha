f21b636a9b78704c99c76aa0a9b34701b79999c9
auroraptor <interestingbutno@yandex.ru>
2022-10-17 12:20:28 +0300

    fix: remove errorHandler

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
-// const { errorHandler } = require('../utils/errorHandler');
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
