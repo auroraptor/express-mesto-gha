4e798349fea4384b05e9124aa43ad5f506537167
auroraptor <interestingbutno@yandex.ru>
2022-10-26 16:44:12 +0300

    Revert "feat: реализовать централизованную обработку ошибок карточек"
    This reverts commit 00f1bf7265782627fec9b139d90b08a533fdbc7b.

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,12 +5,11 @@ const { logNow } = require('../utils/log');
 const { HttpStatusCode } = require('../utils/HttpStatusCode');
 const { HTTP404Error } = require('../errors/HTTP404Error');
 const { HTTP403Error } = require('../errors/HTTP403Error');
-// const { APIError } = require('../errors/APIError');
 
-module.exports.createCard = async (req, res, next) => {
+module.exports.createCard = async (req, res) => {
   try {
     const card = await Card.create({ ...req.body, owner: req.user._id });
-    res.status(HttpStatusCode.OK).send(card);
+    return res.status(HttpStatusCode.OK).send(card);
   } catch (error) {
     // logNow(error.name);
 
@@ -20,18 +19,22 @@ module.exports.createCard = async (req, res, next) => {
     // }
 
     // return res.status(HttpStatusCode.INTERNAL_SERVER).send({ message: 'Тут что-то не так' });
-    next(error);
+    logNow(error.name);
+
+    if (error.name === 'ValidationError') {
+      return res.status(HttpStatusCode.BAD_REQUEST).send({ message: `Ошибка валидации данных: ${error.message.match(regex)}` });
+    }
+
+    return res.status(HttpStatusCode.INTERNAL_SERVER).send({ message: 'Тут что-то не так' });
   }
 };
 
-module.exports.getCards = async (req, res) => {
+module.exports.getCards = async (req, res, next) => {
   try {
     const cards = await Card.find({});
-    return res.status(HttpStatusCode.OK).send({ data: cards });
+    res.status(HttpStatusCode.OK).send({ data: cards });
   } catch (error) {
-    logNow(error.name);
-
-    return res.status(HttpStatusCode.INTERNAL_SERVER).send({ message: 'Тут что-то не так' });
+    next(error);
   }
 };
 
@@ -52,7 +55,7 @@ module.exports.removeCard = async (req, res, next) => {
   }
 };
 
-module.exports.likeCard = async (req, res) => {
+module.exports.likeCard = async (req, res, next) => {
   try {
     const card = await Card.findByIdAndUpdate(
       req.params.cardId,
@@ -60,21 +63,16 @@ module.exports.likeCard = async (req, res) => {
       { new: true },
     );
     if (card === null) {
-      return res.status(HttpStatusCode.NOT_FOUND).send({ message: `Карточка с id ${req.params.id} не найдена` });
+      next(new HTTP404Error(`Карточка с id ${req.params.cardId} не найдена`));
+      return;
     }
-    return res.status(HttpStatusCode.OK).send({ message: '<3' });
+    res.status(HttpStatusCode.OK).send({ message: '<3' });
   } catch (error) {
-    logNow(error.name);
-
-    if (error.name === 'CastError') {
-      return res.status(HttpStatusCode.BAD_REQUEST).send({ message: 'Некорректный запрос' });
-    }
-
-    return res.status(HttpStatusCode.INTERNAL_SERVER).send({ message: 'Тут что-то не так' });
+    next(error);
   }
 };
 
-module.exports.dislikeCard = async (req, res) => {
+module.exports.dislikeCard = async (req, res, next) => {
   try {
     const card = await Card.findByIdAndUpdate(
       req.params.cardId,
@@ -82,16 +80,11 @@ module.exports.dislikeCard = async (req, res) => {
       { new: true },
     );
     if (card === null) {
-      return res.status(HttpStatusCode.NOT_FOUND).json({ message: `Карточка с id ${req.params.id} не найдена` });
+      next(new HTTP404Error(`Карточка с id ${req.params.cardId} не найдена`));
+      return;
     }
-    return res.status(HttpStatusCode.OK).send({ message: '</3' });
+    res.status(HttpStatusCode.OK).send({ message: '</3' });
   } catch (error) {
-    logNow(error.name);
-
-    if (error.name === 'CastError') {
-      return res.status(HttpStatusCode.BAD_REQUEST).send({ message: 'Некорректный запрос' });
-    }
-
-    return res.status(HttpStatusCode.INTERNAL_SERVER).send({ message: 'Тут что-то не так' });
+    next(error);
   }
 };
