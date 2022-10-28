82eda6704687bfded8f3c453f80b29ca5fd884b2
auroraptor <interestingbutno@yandex.ru>
2022-10-28 11:58:53 +0300

    fix: remove custom error

--- errors/BaseError.js
+++
@@ -1,9 +0,0 @@
-class BaseError extends Error {
-  constructor(name, httpCode, message) {
-    super(message);
-    this.name = name;
-    this.statusCode = httpCode;
-  }
-}
-
-module.exports = { BaseError };
