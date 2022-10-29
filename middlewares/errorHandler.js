fb08ad56b710e0ae7715eb5b357a9746e9863804
auroraptor <interestingbutno@yandex.ru>
2022-10-29 19:27:45 +0300

    fix: удалить лог

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,10 +1,7 @@
 const { HttpStatusCode } = require('../utils/HttpStatusCode');
-const { logNow } = require('../utils/log');
 
 module.exports.errorHandler = (err, req, res, next) => {
-  logNow('[HERE WE GO]');
   const { statusCode = HttpStatusCode.INTERNAL_SERVER, message } = err;
-
   res.status(statusCode).send({ message });
   next();
 };
