bdc7a3e05a212a876ef37bd954184da7c7f00c63
auroraptor <interestingbutno@yandex.ru>
2022-10-29 18:14:32 +0300

    fix: передать errorHandler аргументом в качестве колбэк

--- app.js
+++ app.js
@@ -47,9 +47,7 @@ app.use('*', (req, res, next) => {
 
 app.use(errors());
 
-app.use((err, req, res, next) => {
-  errorHandler(err, req, res, next);
-});
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   logNow(`App listening on port ${PORT}`);
