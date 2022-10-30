99cc099655fa7d33f4419e9e73a7f7da65b7973f
auroraptor <interestingbutno@yandex.ru>
2022-10-30 21:23:00 +0300

    fix: брать дефолтный текст ошибки 500

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,6 +2,6 @@ const { HttpStatusCode } = require('../utils/HttpStatusCode');
 
 module.exports.errorHandler = (err, req, res, next) => {
   const { statusCode = HttpStatusCode.INTERNAL_SERVER, message } = err;
-  res.status(statusCode).send({ message });
+  res.status(statusCode).send({ message: statusCode === HttpStatusCode.INTERNAL_SERVER ? 'Internal Server Error (Внутренняя ошибка сервера)' : message });
   next();
 };
